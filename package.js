20412a83430f784619af9f49f5cab059f9020dc5
Jelena Jovanovic <jelena.jjo@gmail.com>
2014-09-21 21:17:30 +0200

    meteor-loading-effect

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  summary: "A set of creative page loading effects that use SVG animations with Snap.svg.",
+  version: "1.0.0",
+  name: "jelena:meteor-loading-effect",
+  git: "https://github.com/jelenajjo/meteor-loading-effect.git"
+});
+
+
+Package.onUse(function (api) {
+  api.use('jquery@1.0.0', 'client');
+
+  api.addFiles('component.css', 'client');
+  api.addFiles('nap.svg-min.js', 'client');
+  api.addFiles('classie.js', 'client');
+  api.addFiles('svgLoader.js', 'client');
+  api.addFiles('img', 'client');
+
+});
